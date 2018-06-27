63a3f061ba6101921b1385b7b3944436b37f9f24
Hai Cao <hai.cao@ffwagency.com>
2018-06-27 22:29:48 +0700

    Update contact

--- wp-content/themes/denholm/init/theme-shortcode.php
+++ wp-content/themes/denholm/init/theme-shortcode.php
@@ -33,3 +33,35 @@ function create_customtax($attrs) {
   ob_end_clean();
   return $content;
 }
+
+// Time bby Locate
+add_shortcode( 'time_by_locate', 'create_time_by_locate' );
+function create_time_by_locate($attrs) {
+  extract(shortcode_atts (array(
+    'tax_name' => ''
+  ), $attrs));
+  ob_start();
+    // Get IP address
+    $ip_address = getenv('HTTP_CLIENT_IP') ?: getenv('HTTP_X_FORWARDED_FOR') ?: getenv('HTTP_X_FORWARDED') ?: getenv('HTTP_FORWARDED_FOR') ?: getenv('HTTP_FORWARDED') ?: getenv('REMOTE_ADDR');
+
+    // Get JSON object
+    $jsondata = file_get_contents("http://timezoneapi.io/api/ip/?" . $ip_address);
+
+    // Decode
+    $data = json_decode($jsondata, true);
+
+    print_r($data);
+    /*// Request OK?
+    if($data['meta']['code'] == '200'){
+
+      // Example: Get the city parameter
+      echo "City: " . $data['data']['city'] . "<br>";
+
+      // Example: Get the users time
+      echo "Time: " . $data['data']['datetime']['date_time_txt'] . "<br>";
+
+    }*/
+  $content = ob_get_contents();
+  ob_end_clean();
+  return $content;
+}
