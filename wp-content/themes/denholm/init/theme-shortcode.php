9afe569202fb5051d2e65c06dddc883f6d5742aa
Bien Vu <bien.vu@ffwagency.com>
2018-07-03 16:14:59 +0700

    DH: update style fix bug QA

--- wp-content/themes/denholm/init/theme-shortcode.php
+++ wp-content/themes/denholm/init/theme-shortcode.php
@@ -41,17 +41,17 @@ function create_time_by_locate($attrs) {
     'tax_name' => ''
   ), $attrs));
   ob_start();
-    // Get IP address
-    //$ip_address = getenv('HTTP_CLIENT_IP') ?: getenv('HTTP_X_FORWARDED_FOR') ?: getenv('HTTP_X_FORWARDED') ?: getenv('HTTP_FORWARDED_FOR') ?: getenv('HTTP_FORWARDED') ?: getenv('REMOTE_ADDR');
-
-    // Get JSON object
-    $jsondata = file_get_contents("http://timezoneapi.io/api/ip/?");
-
     // // Get IP address
-    // $ip_address = getenv('HTTP_CLIENT_IP') ?: getenv('HTTP_X_FORWARDED_FOR') ?: getenv('HTTP_X_FORWARDED') ?: getenv('HTTP_FORWARDED_FOR') ?: getenv('HTTP_FORWARDED') ?: getenv('REMOTE_ADDR');
+    // //$ip_address = getenv('HTTP_CLIENT_IP') ?: getenv('HTTP_X_FORWARDED_FOR') ?: getenv('HTTP_X_FORWARDED') ?: getenv('HTTP_FORWARDED_FOR') ?: getenv('HTTP_FORWARDED') ?: getenv('REMOTE_ADDR');
     //
     // // Get JSON object
-    // $jsondata = file_get_contents("http://timezoneapi.io/api/ip/"? . $ip_address);
+    // $jsondata = file_get_contents("http://timezoneapi.io/api/ip/?");
+
+    // Get IP address
+    $ip_address = getenv('HTTP_CLIENT_IP') ?: getenv('HTTP_X_FORWARDED_FOR') ?: getenv('HTTP_X_FORWARDED') ?: getenv('HTTP_FORWARDED_FOR') ?: getenv('HTTP_FORWARDED') ?: getenv('REMOTE_ADDR');
+
+    // Get JSON object
+    $jsondata = file_get_contents("http://timezoneapi.io/api/ip/"? . $ip_address);
 
     // Decode
     $data = json_decode($jsondata, true);
